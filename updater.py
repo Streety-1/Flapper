b75eeffa61b5320df14f221695f2470c2bd18684
Streety <88004408+Streety-1@users.noreply.github.com>
2024-09-01 21:55:58 +0100

    Update updater.py

--- updater.py
+++ updater.py
@@ -1,6 +1,31 @@
 # Download Latest version from github repo
 # https://github.com/Streety-1/Flapper-Nought.git
 
-import time
-print("HELLO IM UPDATER")
-time.sleep(5)
+#-------------------Prequisit Installer-------------------#
+
+try:
+    urllib.request.urlopen(urltocheckwifi)
+    print("| Updating pip |")
+    systemCmd('python.exe -m pip install --upgrade pip --quiet')
+    print("| Updating modules |")
+
+    #install modules
+    for x in requiredmodules:
+        try:
+            import x
+            print("\033[2J\033[H", end="", flush=false)
+        except ImportError:
+            pipinstall(x)
+
+    print("| Updating dbus |")
+    systemCmd('sudo apt install python-dbus --quiet')
+    
+    #get script from git and launch
+    
+    
+    
+    clear()
+
+except urllib.error.URLError:
+    print("No wifi")
+
