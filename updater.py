a6931a7d02dde7f0cbe2bc79f5d4abc264eb5cb8
Streety <88004408+Streety-1@users.noreply.github.com>
2024-09-01 21:59:51 +0100

    Update updater.py

--- updater.py
+++ updater.py
@@ -1,6 +1,10 @@
 # Download Latest version from github repo
 # https://github.com/Streety-1/Flapper-Nought.git
 
+import subprocess
+import sys
+import urllib.request
+import requests
 
 urltocheckwifi = "https://www.google.com/"
 
