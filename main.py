04e3650333f504998c056840c0d614864f019d4e
Streety-1 <f.logan060@gmail.com>
2024-09-05 19:20:12 +0100

    upd

--- main.py
+++ main.py
@@ -41,16 +41,29 @@ def main(stdscr):
     # Clear screen
     stdscr.clear()
 
+    # Custom text to display at the top
+    top_text = b+"""
+  ___  _                             
+ | __|| | __ _  _ __  _ __  ___  _ _ 
+ | _| | |/ _` || '_ \| '_ \/ -_)| '_|
+ |_|  |_|\__,_|| .__/| .__/\___||_|  
+               |_|   |_|                                       
+    """
+
     # List of menu options with associated color pairs
     menu = [
-        ("AP scanner", 2),
-        ("Update", 3),
+        ("AP scanner", 1),
+        ("Update", 4),
         ("Shutdown", 4)
     ]
     current_row = 0
 
     while True:
         stdscr.clear()
+
+        # Display the custom top text
+        stdscr.addstr(0, 0, top_text, curses.A_BOLD | curses.color_pair(4))
+
         h, w = stdscr.getmaxyx()
 
         # Display the menu
