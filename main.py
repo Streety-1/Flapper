18f33bc54d7023d7c064c42160a438b2898c06e9
Streety-1 <f.logan060@gmail.com>
2024-09-05 19:03:49 +0100

    upd

--- main.py
+++ main.py
@@ -3,8 +3,10 @@
 
 #------------------------------------IMPORTS------------------------------------#
 import os
+import subprocess
 import time
 import datetime
+import curses
 
 #------------------------------------VALUES------------------------------------#
 timenow = datetime.datetime.now()
@@ -20,26 +22,53 @@ def run_python(name):
     systemCmd("clear")
     exec(open(name).read())
 
-from InquirerPy import inquirer
-from InquirerPy.base.control import Choice
-from InquirerPy.separator import Separator
-from InquirerPy import inquirer
-
 #------------------------------------START------------------------------------#
 
-def update():
-    checkupd = inquirer.confirm(message="This will install the latest build.").execute()
-
-    if checkupd:
-        systemCmd("cd /")
-        systemCmd("sudo python3 flappernought/FlapperUpdater.py")
-    if not checkupd:
-        main_menu()
+#def update():
 
 def ap_scan():
     systemCmd("clear")
     time.sleep(2)
     run_python('ap-scanner.py')
+
+def main(stdscr):
+    # Clear screen
+    stdscr.clear()
+
+    # List of menu options
+    menu = ["Option 1", "Option 2", "Option 3", "Option 4"]
+    current_row = 0
+
+    while True:
+        stdscr.clear()
+        h, w = stdscr.getmaxyx()
+
+        # Display the menu
+        for idx, row in enumerate(menu):
+            x = w // 2 - len(row) // 2
+            y = h // 2 - len(menu) // 2 + idx
+            if idx == current_row:
+                stdscr.attron(curses.color_pair(1))
+                stdscr.addstr(y, x, row)
+                stdscr.attroff(curses.color_pair(1))
+            else:
+                stdscr.addstr(y, x, row)
+
+        stdscr.refresh()
+
+        key = stdscr.getch()
+
+        # Navigate the menu
+        if key == curses.KEY_DOWN:
+            current_row = (current_row + 1) % len(menu)
+        elif key == curses.KEY_UP:
+            current_row = (current_row - 1) % len(menu)
+        elif key == ord('\n'):  # Enter key
+            stdscr.addstr(h // 2 + len(menu), w // 2 - len("You selected '{}'".format(menu[current_row])) // 2, "You selected '{}'".format(menu[current_row]))
+            stdscr.refresh()
+            stdscr.getch()
+        elif key == 27:  # ESC key to exit
+            break
 
 def main_menu():
     systemCmd("clear")
@@ -51,22 +80,7 @@ def main_menu():
                |_|   |_|                                       
     """)
 
-    Selection = inquirer.select(
-        message="Please select an option:",
-        choices=[
-            "AP scanner",
-            "Update"
-        ],
-        default=None,
-    ).execute()
-    selected = Selection
-    
-    print(b+'Please select an option: ')
-
-    if selected == "Update":
-        update()
-    if selected == "AP scanner":
-        ap_scan()
+    curses.wrapper(main)
 
 
 main_menu()
